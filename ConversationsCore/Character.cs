bd61ad0fa8d069b19db6a33044f2e0fcdde955e7
bob <bob@SPEECHTEST>
2016-05-08 14:37:23 -0400

    Finished initial classes

---
+++ ConversationsCore/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversationsCore
+{
+    public class Character
+    {
+        public int RelationshipStrength { get; set; }
+
+        public Dictionary<string, bool> ContextFlags { get; set; }
+
+        public List<State> StateList { get; set; }
+
+        public State CurrentState { get; set; }
+
+
+    }
+}
---
+++ ConversationsCore/State.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversationsCore
+{
+    public class State
+    {
+        public int id { get; set; }
+
+        public string LUISAppId { get; set; }
+
+        public Stack<State> FallbackStateStack { get; set; }
+
+        public Dictionary<Intent, Response> IntentMap { get; set; }
+
+    }
+}
