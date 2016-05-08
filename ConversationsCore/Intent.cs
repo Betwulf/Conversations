2f52fd477ce2c7c8135d535901a452b1de1fd7de
bob <bob@SPEECHTEST>
2016-05-08 14:29:28 -0400

    Added some classes

---
+++ ConversationsCore/Intent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversationsCore
+{
+    public class Intent
+    {
+        public int id { get; set; }
+
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Type and keyword associated with this intent
+        /// </summary>
+        public Dictionary<string, string> EntityDictionary { get; set; }
+
+    }
+}
---
+++ ConversationsCore/Response.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversationsCore
+{
+    public class Response
+    {
+        public int id { get; set; }
+
+        public string WaveFilename { get; set; }
+
+
+    }
+}
