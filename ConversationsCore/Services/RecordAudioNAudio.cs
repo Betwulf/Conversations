bad73433766ef0edc0be398de1f7d5d1712cd650
bob <bob@SPEECHTEST>
2016-05-13 19:55:47 -0400

    finished first pass at NAudio Record

--- ConversationsCore/Services/RecordAudioNAudio.cs
+++ ConversationsCore/Services/RecordAudioNAudio.cs
@@ -25,7 +25,7 @@ namespace ConversationsCore.Services
         WaveIn waveIn;
         readonly SampleAggregator sampleAggregator;
         UnsignedMixerControl volumeControl;
-        double desiredVolume = 100;
+        double MicrophoneLevel = 100;
         WaveFileWriter writer;
 
         public bool IsRecording { get; set; }
@@ -42,22 +42,118 @@ namespace ConversationsCore.Services
         {
             if (IsRecording)
             {
-                throw new InvalidOperationException("Can't begin monitoring while we are in this state: " + recordingState.ToString());
+                throw new InvalidOperationException("FAIL - Already recording");
             }
             waveIn = new WaveIn();
-            waveIn.DeviceNumber = recordingDevice;
+            if (WaveIn.DeviceCount < 1)
+            {
+                throw new InvalidOperationException("Cant find a device...");
+            }
+            waveIn.DeviceNumber = 0;
             waveIn.DataAvailable += OnDataAvailable;
             waveIn.RecordingStopped += OnRecordingStopped;
-            waveIn.WaveFormat = recordingFormat;
+            waveIn.WaveFormat = RecordingFormat;
             waveIn.StartRecording();
             TryGetVolumeControl();
             IsRecording = true;
+
+            return true;
         }
 
         public bool StopRecordingAudioAsync()
         {
+            if (IsRecording)
+            {
+                IsRecording = false;
+                waveIn.StopRecording();
+            }
             return true;
         }
-        
+
+
+
+        private void TryGetVolumeControl()
+        {
+            int waveInDeviceNumber = waveIn.DeviceNumber;
+            if (Environment.OSVersion.Version.Major >= 6) // Vista and over
+            {
+                var mixerLine = waveIn.GetMixerLine();
+                //new MixerLine((IntPtr)waveInDeviceNumber, 0, MixerFlags.WaveIn);
+                foreach (var control in mixerLine.Controls)
+                {
+                    if (control.ControlType == MixerControlType.Volume)
+                    {
+                        this.volumeControl = control as UnsignedMixerControl;
+                        volumeControl.Percent = MicrophoneLevel;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                var mixer = new Mixer(waveInDeviceNumber);
+                foreach (var destination in mixer.Destinations
+                    .Where(d => d.ComponentType == MixerLineComponentType.DestinationWaveIn))
+                {
+                    foreach (var source in destination.Sources
+                        .Where(source => source.ComponentType == MixerLineComponentType.SourceMicrophone))
+                    {
+                        foreach (var control in source.Controls
+                            .Where(control => control.ControlType == MixerControlType.Volume))
+                        {
+                            volumeControl = control as UnsignedMixerControl;
+                            volumeControl.Percent = MicrophoneLevel;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        private void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            byte[] buffer = e.Buffer;
+            int bytesRecorded = e.BytesRecorded;
+            WriteToFile(buffer, bytesRecorded);
+
+            for (int index = 0; index < e.BytesRecorded; index += 2)
+            {
+                short sample = (short)((buffer[index + 1] << 8) |
+                                        buffer[index + 0]);
+                float sample32 = sample / 32768f;
+                sampleAggregator.Add(sample32);
+            }
+        }
+
+        private void WriteToFile(byte[] buffer, int bytesRecorded)
+        {
+            long maxFileLength = this.RecordingFormat.AverageBytesPerSecond * 60;
+
+            if (!IsRecording)
+            {
+                var toWrite = (int)Math.Min(maxFileLength - writer.Length, bytesRecorded);
+                if (toWrite > 0)
+                {
+                    writer.Write(buffer, 0, bytesRecorded);
+                }
+                else
+                {
+                    StopRecordingAudioAsync();
+                }
+            }
+        }
+
+
+
+        void OnRecordingStopped(object sender, StoppedEventArgs e)
+        {
+            IsRecording = false;
+            writer.Dispose();
+            FinishedRecordingEvent(this, null);
+        }
+
+
     }
 }
